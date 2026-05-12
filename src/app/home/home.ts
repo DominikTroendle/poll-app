88d377d64ed23e8e5a191ea8569f13e5ab550202
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-12 21:52:21 +0200

    feat: added survey section to home with basic layout

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { HeroVisual } from "./hero-section/hero-visual/hero-visual";
-import { ButtonPrimary } from "../shared/button-primary/button-primary";
+import { HeroVisual } from './hero-section/hero-visual/hero-visual';
+import { ButtonPrimary } from '../shared/button-primary/button-primary';
+import { EndingSoon } from './ending-soon/ending-soon';
+import { SurveyOverview } from './survey-overview/survey-overview';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroVisual, ButtonPrimary],
+  imports: [HeroVisual, ButtonPrimary, EndingSoon, SurveyOverview],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  homeButtonText = "New survey";
+  homeButtonText = 'New survey';
 }
