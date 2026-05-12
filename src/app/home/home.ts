a3c1d9b3aed9c67053002b787b84ec2bde795fa6
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-12 17:06:55 +0200

    feat: added wrapper elements for sectioned styling

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroVisual } from "./hero-section/hero-visual/hero-visual";
+import { ButtonPrimary } from "../shared/button-primary/button-primary";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroVisual],
+  imports: [HeroVisual, ButtonPrimary],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
