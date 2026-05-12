0797ea95ad2e6c1fa54c1128a26408cd6a990506
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-12 22:17:44 +0200

    refactor: outsourced hero-section elements and their styling to hero-section component

--- src/app/home/hero-section/hero-section.ts
+++ src/app/home/hero-section/hero-section.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroVisual } from './hero-visual/hero-visual';
+import { ButtonPrimary } from '../../shared/button-primary/button-primary';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [HeroVisual],
+  imports: [HeroVisual, ButtonPrimary],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.scss',
 })
-export class HeroSection {}
+export class HeroSection {
+  homeButtonText = 'New survey';
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,13 +3,14 @@ import { HeroVisual } from './hero-section/hero-visual/hero-visual';
 import { ButtonPrimary } from '../shared/button-primary/button-primary';
 import { EndingSoon } from './ending-soon/ending-soon';
 import { SurveyOverview } from './survey-overview/survey-overview';
+import { HeroSection } from "./hero-section/hero-section";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroVisual, ButtonPrimary, EndingSoon, SurveyOverview],
+  imports: [EndingSoon, SurveyOverview, HeroSection],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  homeButtonText = 'New survey';
+  
 }
