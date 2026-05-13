6d7589cd9b105f41af0c25a2d1446626fff646e0
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-13 22:54:40 +0200

    feat: added basic structure to survey-overview component

--- src/app/home/survey-overview/survey-overview.ts
+++ src/app/home/survey-overview/survey-overview.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CategoryDropdown } from "../../shared/category-dropdown/category-dropdown";
 
 @Component({
   selector: 'app-survey-overview',
-  imports: [],
+  imports: [CategoryDropdown],
   templateUrl: './survey-overview.html',
   styleUrl: './survey-overview.scss',
 })
