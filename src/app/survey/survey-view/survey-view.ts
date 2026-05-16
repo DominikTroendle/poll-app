cca579eda62bf24bc9cde6b869196a767ee43f00
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-16 12:14:43 +0200

    feat: added survey-form with ts imports to survey-view component

--- src/app/survey/survey-view/survey-view.ts
+++ src/app/survey/survey-view/survey-view.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SurveyStatus } from '../../shared/survey-status/survey-status';
+import { SurveyForm } from "../survey-form/survey-form";
 
 @Component({
   selector: 'app-survey-view',
-  imports: [SurveyStatus],
+  imports: [SurveyStatus, SurveyForm],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
