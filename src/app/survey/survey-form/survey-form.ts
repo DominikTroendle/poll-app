a5e6b6f1faeb51938fb152f9ff57dcf382f8c0fd
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-17 13:54:12 +0200

    refactor: remove unused SurveyInput import

--- src/app/survey/survey-form/survey-form.ts
+++ src/app/survey/survey-form/survey-form.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { SurveyInput } from '../survey-input/survey-input';
 import { SurveyQuestion } from '../survey-question/survey-question';
 
 @Component({
   selector: 'app-survey-form',
-  imports: [SurveyQuestion, SurveyInput],
+  imports: [SurveyQuestion],
   templateUrl: './survey-form.html',
   styleUrl: './survey-form.scss',
 })
