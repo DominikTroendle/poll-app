608d81c2dfea0646534cd7d73ee67de84c930f7e
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-17 09:09:30 +0200

    feat: added survey-results component with ts imports to survey-view component

--- src/app/survey/survey-view/survey-view.ts
+++ src/app/survey/survey-view/survey-view.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SurveyStatus } from '../../shared/survey-status/survey-status';
 import { SurveyForm } from '../survey-form/survey-form';
+import { SurveyResults } from '../survey-results/survey-results';
 
 @Component({
   selector: 'app-survey-view',
-  imports: [SurveyStatus, SurveyForm],
+  imports: [SurveyStatus, SurveyForm, SurveyResults],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
