df3f514341299d37ddd3292480a564e500afc77f
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-17 07:50:10 +0200

    feat: generated survey-question component

---
+++ src/app/survey/survey-question/survey-question.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-question',
+  imports: [],
+  templateUrl: './survey-question.html',
+  styleUrl: './survey-question.scss',
+})
+export class SurveyQuestion {}
