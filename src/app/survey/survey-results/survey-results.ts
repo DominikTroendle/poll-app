f9f6741f4f7948179ec6202241990d4036211394
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-17 12:09:25 +0200

    feat: added button logic to toggle visibility of survey-results container

--- src/app/survey/survey-results/survey-results.ts
+++ src/app/survey/survey-results/survey-results.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   templateUrl: './survey-results.html',
   styleUrl: './survey-results.scss',
 })
-export class SurveyResults {}
+export class SurveyResults {
+  isOpen = true;
+  buttonText = "Close results";
+
+  get getImgSrc():string {
+    return this.isOpen ? 'svg/arrow-dropdown-open.svg' : 'svg/arrow-dropdown.svg';
+  }
+
+  toggleResultsVisibility():void {
+    this.isOpen = !this.isOpen;
+    this.buttonText = this.isOpen ? "Close results" : "See results" ;
+  }
+}
