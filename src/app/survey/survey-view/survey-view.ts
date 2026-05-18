40efb6b47fd62a1af06879ef5276a66852c80d00
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-18 08:52:28 +0200

    feat: add routerLink to survey-views button

--- src/app/survey/survey-view/survey-view.ts
+++ src/app/survey/survey-view/survey-view.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SurveyStatus } from '../../shared/survey-status/survey-status';
 import { SurveyForm } from '../survey-form/survey-form';
 import { SurveyResults } from '../survey-results/survey-results';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-survey-view',
-  imports: [SurveyStatus, SurveyForm, SurveyResults],
+  imports: [SurveyStatus, SurveyForm, SurveyResults, RouterLink],
   templateUrl: './survey-view.html',
   styleUrl: './survey-view.scss',
 })
