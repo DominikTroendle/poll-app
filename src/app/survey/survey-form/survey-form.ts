ef089dbd18553cbb8a489303eb839426c7467111
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-19 07:05:54 +0200

    reafctor: replace button-element with button-primary component

--- src/app/survey/survey-form/survey-form.ts
+++ src/app/survey/survey-form/survey-form.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SurveyQuestion } from '../survey-question/survey-question';
+import { ButtonPrimary } from '../../shared/button-primary/button-primary';
 
 @Component({
   selector: 'app-survey-form',
-  imports: [SurveyQuestion],
+  imports: [SurveyQuestion, ButtonPrimary],
   templateUrl: './survey-form.html',
   styleUrl: './survey-form.scss',
 })
-export class SurveyForm {}
+export class SurveyForm {
+  completeSurveyButtonText = 'Complete';
+}
