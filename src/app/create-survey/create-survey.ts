c9bff45ceb55e5622630a8a3bdf990c5b656a6e2
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-19 07:12:45 +0200

    feat: add section element and button-primary component

--- src/app/create-survey/create-survey.ts
+++ src/app/create-survey/create-survey.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonPrimary } from '../shared/button-primary/button-primary';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [],
+  imports: [ButtonPrimary],
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
-export class CreateSurvey {}
+export class CreateSurvey {
+  createSurveyButtonText = 'Publish';
+}
