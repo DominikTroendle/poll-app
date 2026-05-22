95d28e91b1a3d600a55b3f42043e6d5e52daae20
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-22 08:01:48 +0200

    styling: add basic layout structure

--- src/app/create-survey/create-survey.ts
+++ src/app/create-survey/create-survey.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonPrimary } from '../shared/button-primary/button-primary';
+import { SurveyStatus } from '../shared/survey-status/survey-status';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [ButtonPrimary],
+  imports: [ButtonPrimary, SurveyStatus, RouterLink],
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
