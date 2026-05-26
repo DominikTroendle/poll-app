4639be796988c76a9a4e35b9ea690fba410fafdc
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-26 07:48:44 +0200

    feat: add create-survey-questions components elements

--- src/app/create-survey/create-survey-question/create-survey-question.ts
+++ src/app/create-survey/create-survey-question/create-survey-question.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CreateSurveyField } from '../create-survey-field/create-survey-field';
 
 @Component({
   selector: 'app-create-survey-question',
-  imports: [],
+  imports: [CreateSurveyField],
   templateUrl: './create-survey-question.html',
   styleUrl: './create-survey-question.scss',
 })
