33aaf27ca3ea4b7f4f3f44ff158d46cc9e37cadc
Dominik Tröndle <domi.troendle@gmail.com>
2026-05-26 07:48:02 +0200

    feat: add create-sruvey-question component, add imports

--- src/app/create-survey/create-survey.ts
+++ src/app/create-survey/create-survey.ts
@@ -4,10 +4,18 @@ import { SurveyStatus } from '../shared/survey-status/survey-status';
 import { RouterLink } from '@angular/router';
 import { CreateSurveyField } from './create-survey-field/create-survey-field';
 import { CategoryDropdown } from '../shared/category-dropdown/category-dropdown';
+import { CreateSurveyQuestion } from './create-survey-question/create-survey-question';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [ButtonPrimary, SurveyStatus, RouterLink, CreateSurveyField, CategoryDropdown],
+  imports: [
+    ButtonPrimary,
+    SurveyStatus,
+    RouterLink,
+    CreateSurveyField,
+    CategoryDropdown,
+    CreateSurveyQuestion,
+  ],
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
