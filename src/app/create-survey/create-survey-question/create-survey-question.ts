b263bf64ae80c184d64aca2553cc5695382ab2a8
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-02 10:13:17 +0200

    feat: add getAnswerField function to return a object matching the CreateSurveyConfig to allow data to be passed to crete-survey-fields input, refactor: interchanged fieldTitle and fieldLayout data binding with new field data binding

--- src/app/create-survey/create-survey-question/create-survey-question.ts
+++ src/app/create-survey/create-survey-question/create-survey-question.ts
@@ -1,5 +1,8 @@
 import { Component, input } from '@angular/core';
-import { CreateSurveyField } from '../create-survey-field/create-survey-field';
+import {
+  CreateSurveyField,
+  CreateSurveyFieldConfig,
+} from '../create-survey-field/create-survey-field';
 
 @Component({
   selector: 'app-create-survey-question',
@@ -9,11 +12,28 @@ import { CreateSurveyField } from '../create-survey-field/create-survey-field';
 })
 export class CreateSurveyQuestion {
   questionNumber = input<number>(1);
-  answers = [{ id: 'A.' }, { id: 'B.' }];
+  answers = [
+    { id: 'a', label: 'A.' },
+    { id: 'b', label: 'B.' },
+  ];
+
+  getAnswerField(answer: { id: string; label: string }): CreateSurveyFieldConfig {
+    const questionNumber = this.questionNumber;
+    return {
+      id: `question-${questionNumber}-answer-${answer.id}`,
+      name: `question-${questionNumber}-answer-${answer.id}`,
+      title: answer.label,
+      type: 'text',
+      layout: 'answer',
+    };
+  }
 
   addAnswer(): void {
-    const lastAnswerId = this.answers.at(-1)!.id;
+    const lastAnswerId = this.answers.at(-1)!.label;
     const nextCharCode = String.fromCharCode(lastAnswerId.charCodeAt(0) + 1);
-    this.answers = [...this.answers, { id: `${nextCharCode}.` }];
+    this.answers = [
+      ...this.answers,
+      { id: `${nextCharCode.toLowerCase}`, label: `${nextCharCode}.` },
+    ];
   }
 }
