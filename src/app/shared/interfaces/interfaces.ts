4767aaddf872423e3ea91f128474c038d1be394e
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-03 19:18:53 +0200

    feat: add interface file, add interfaces for SurveyMetaData, SurveyWithQuestions, Answer and Question as well as type for categories

---
+++ src/app/shared/interfaces/interfaces.ts
@@ -0,0 +1,33 @@
+export type Category =
+  | 'Team Activities'
+  | 'Health & Wellness'
+  | 'Gaming & Entertainment'
+  | 'Education & Learning'
+  | 'Lifestyle & Preferences'
+  | 'Technology & Innovation';
+
+export interface SurveyMetaData {
+  id: number;
+  title: string;
+  description: string;
+  category: Category;
+  ends_at: string;
+}
+
+export interface Answer {
+  id: number;
+  question_id: number;
+  answer: string;
+}
+
+export interface Question {
+  id: number;
+  survey_id: number;
+  question: string;
+  allow_multiple_answers: boolean;
+  answer_options: Answer[];
+}
+
+export interface SurveyWithQuestions extends SurveyMetaData {
+  questions: Question[];
+}
--- src/app/shared/services/supabase.ts
+++ src/app/shared/services/supabase.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
+import { SurveyMetaData } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -8,4 +9,6 @@ export class Supabase {
   supabaseUrl = 'https://ejrqzxvqbqlwupmkpnnc.supabase.co';
   supabaseKey = 'sb_publishable_yplhzWQKpzSXMqWXJ638YA_bXmsnyzj';
   supabase = createClient(this.supabaseUrl, this.supabaseKey);
+
+  surveys = signal<SurveyMetaData[]>([]);
 }
