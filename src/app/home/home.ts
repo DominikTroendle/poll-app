bae16f4213d28d87178652daa668f2cfae6e5149
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-08 20:41:01 +0200

    feat: add function to get surveys from supabase

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HeroVisual } from './hero-section/hero-visual/hero-visual';
-import { ButtonPrimary } from '../shared/button-primary/button-primary';
 import { EndingSoon } from './ending-soon/ending-soon';
 import { SurveyOverview } from './survey-overview/survey-overview';
 import { HeroSection } from './hero-section/hero-section';
--- src/app/shared/services/supabase.ts
+++ src/app/shared/services/supabase.ts
@@ -11,4 +11,16 @@ export class Supabase {
   supabase = createClient(this.supabaseUrl, this.supabaseKey);
 
   surveys = signal<SurveyMetaData[]>([]);
+
+  async getSurveys() {
+    const { data: surveys, error } = await this.supabase
+      .from('surveys')
+      .select('*')
+      .order('ends_at', { ascending: true });
+    if (error) {
+      console.error('Supabase error:', error);
+      return;
+    }
+    this.surveys.set(surveys ?? []);
+  }
 }
