831d5d4135da6723cc21a607210c05653e1685e6
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-19 10:02:25 +0200

    feat: add for-loop rendering of survey-list items depending on computed active or passive surveys

--- src/app/home/survey-list/survey-list.ts
+++ src/app/home/survey-list/survey-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { SurveyListItem } from '../../shared/survey-list-item/survey-list-item';
+import { Supabase } from '../../shared/services/supabase';
+import { SurveyMetaData } from '../../shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-survey-list',
@@ -7,4 +9,22 @@ import { SurveyListItem } from '../../shared/survey-list-item/survey-list-item';
   templateUrl: './survey-list.html',
   styleUrl: './survey-list.scss',
 })
-export class SurveyList {}
+export class SurveyList {
+  supabase = inject(Supabase);
+  activeSurveys = computed(() => this.filterSurveys('active'));
+  pastSurveys = computed(() => this.filterSurveys('past'));
+
+  filterSurveys(status: string): SurveyMetaData[] {
+    const today = new Date();
+    const todayString = [
+      today.getFullYear(),
+      String(today.getMonth() + 1).padStart(2, '0'),
+      String(today.getDate()).padStart(2, '0'),
+    ].join('-');
+    return this.supabase
+      .surveys()
+      .filter((survey) =>
+        status === 'active' ? survey.ends_at >= todayString : survey.ends_at <= todayString,
+      );
+  }
+}
