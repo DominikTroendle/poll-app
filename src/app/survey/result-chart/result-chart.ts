256049171c5984f4fb1a918e428794203b2054b3
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-24 23:05:18 +0200

    feat: add answerOptions index as resultId input to result-chart, add function to convert input resultId to corresponding letter, add converted letter to httml template

--- src/app/survey/result-chart/result-chart.ts
+++ src/app/survey/result-chart/result-chart.ts
@@ -9,9 +9,14 @@ import { Component, input } from '@angular/core';
 export class ResultChart {
   selectionCount = input.required<number>();
   participants = input.required<number>();
+  resultId = input.required<number>();
 
   calculatePercent(): number {
     const percent = (this.selectionCount() / this.participants()) * 100;
     return Math.round(percent);
+  }
+
+  getAnswerLetter(): string {
+    return String.fromCharCode(65 + this.resultId());
   }
 }
