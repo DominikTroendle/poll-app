116c0b35296488bd7337be0335ec8067fd269b87
Dominik Tröndle <domi.troendle@gmail.com>
2026-07-25 08:53:17 +0200

    feat: add storeSelectedAnswer function to store received output from survey-input and store it in a local selectedAnswerIds array

--- src/app/survey/survey-question/survey-question.ts
+++ src/app/survey/survey-question/survey-question.ts
@@ -1,6 +1,6 @@
 import { Component, input } from '@angular/core';
 import { SurveyInput } from '../survey-input/survey-input';
-import { Question } from '../../shared/interfaces/interfaces';
+import { Question, SelectedAnswer } from '../../shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-survey-question',
@@ -12,4 +12,12 @@ export class SurveyQuestion {
   question = input.required<Question>();
   questionId = input.required<number>();
   disabled = input.required<boolean>();
+
+  selectedAnswerIDs: number[] = [];
+
+  storeSelectedAnswer(selection: SelectedAnswer): void {
+    this.question().allow_multiple_answers
+      ? (this.selectedAnswerIDs = [...this.selectedAnswerIDs, selection.answerOptionId])
+      : (this.selectedAnswerIDs = [selection.answerOptionId]);
+  }
 }
